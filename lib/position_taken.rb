936fb47d1f32146253a4ff32d429d8eeeac4468b
Josh Stegmeier <josh@steg.me>
2018-03-24 03:34:12 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,12 @@
 # code your #position_taken? method here!
 
+board = ["", " ", "   ", " ", " ", " ", " ", " ", " "]
+
 def position_taken?(board, index)
   val = board[index].strip
   if val != ""
     return true
   end
 end
+
+position_taken?(board, 0)

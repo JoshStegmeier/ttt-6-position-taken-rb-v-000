6631bf19b31e77ae9609e7ecd9677c13872d6f99
Josh Stegmeier <josh@steg.me>
2018-03-24 03:46:32 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,5 @@
 # code your #position_taken? method here!
 
-board = ["X", " ", "   ", " ", " ", " ", " ", " ", " "]
-
 def position_taken?(board, index)
   if board[index].strip == "" || board[index] == nil
     false
